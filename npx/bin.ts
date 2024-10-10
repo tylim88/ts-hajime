43775ffaaca21f88ffaf18c8efb34d2c6e0bcaeb
tylim <teikyee.lim@it.destijltech.com>
2024-10-10 16:46:51 +0800

    setup npx

---
+++ npx/bin.ts
@@ -0,0 +1,2 @@
+#!/usr/bin/env node
+export const a = 1
---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+	entry: ['npx/bin.ts'],
+	publicDir: false,
+	clean: true,
+	minify: true,
+	format: ['cjs'],
+	tsconfig: './tsconfig.npx.json',
+})
