ff8dab26d3847fc5334884e3baa8105d10e363a9
tylim <teikyee.lim@it.destijltech.com>
2024-10-10 14:56:30 +0800

    fix eslint and add fix on save

--- src/isEven.test.ts
+++ src/isEven.test.ts
@@ -2,6 +2,6 @@ import { it, expect } from 'vitest'
 import { isEven } from './isEven'
 
 it('test isEven', () => {
-	// expect(isEven(2)).toBe(true)
-	// expect(isEven(1)).toBe(false)
+	expect(isEven(2)).toBe(true)
+	expect(isEven(1)).toBe(false)
 })
