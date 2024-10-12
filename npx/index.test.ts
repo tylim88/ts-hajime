004758a10974564b39670c2b4f9625e44edf5639
tylim <limteikyee@gmail.com>
2024-10-12 23:04:59 +0800

    path now working in both cjs and mjs

--- npx/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, it } from 'vitest'
-import { execa } from 'execa'
-
-it('test cli', async () => {
-	const { stdout } = await execa`npm run npx my-app`
-	expect(stdout).toContain('done, have fun!')
-})
--- npx/index.ts
+++ npx/index.ts
@@ -1,25 +1,31 @@
 #!/usr/bin/env node
 import { cli } from 'cleye'
 import fs from 'fs-extra'
-import path from 'path'
+import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 import { execaSync } from 'execa'
+
+const getDirname = () => {
+	if (typeof __dirname === 'undefined') {
+		return dirname(fileURLToPath(import.meta.url))
+	} else {
+		return __dirname
+	}
+}
 
 try {
 	const argv = cli({
 		name: 'ts-hajime',
 		parameters: ['<app name>'],
 	})
-	const __filename = fileURLToPath(import.meta.url)
-	const __dirname = path.dirname(__filename)
-	console.log('copying...')
+	console.log('Copying...')
 	fs.copySync(
-		path.resolve(__dirname, '../template'),
-		path.resolve(process.cwd(), argv._.appName),
+		resolve(getDirname(), '../template'),
+		resolve(process.cwd(), argv._.appName),
 	)
-	console.log('installing node modules...')
+	console.log('Copy complete! Installing node modules...')
 	execaSync('npm', ['i'], { cwd: argv._.appName })
-	console.log('done, have fun!')
+	console.log('Installation complete! Have fun!')
 } catch (e) {
 	console.error(e)
 }
