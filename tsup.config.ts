0a63dc2abe45240302893afeae0b3817d4487441
tylim <limteikyee@gmail.com>
2026-04-22 09:46:29 +0800

    disable minification

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	entry: ['npx/index.ts'],
 	clean: true,
-	minify: true,
 	format: ['cjs'],
 	tsconfig: 'tsconfig.npx.json',
 	outDir: 'bin',
